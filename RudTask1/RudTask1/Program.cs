0b7145f3644696223b1cfd6fd06c602785834c0a
AlexRud <alexrud1984@gmail.com>
2016-11-13 18:57:13 +0200

    add previous hometasks

---
+++ RudTask1/RudTask1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RudTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int CustomerValue = 0;
+            int CustomerDivider = 0;
+            Console.Write("Hello! Enter your Value and press Enter.\n");
+            CustomerValue=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter possible divider for the Value and press Enter.\n");
+            CustomerDivider= Convert.ToInt32(Console.ReadLine());
+            if (Convert.ToBoolean(CustomerValue % CustomerDivider))
+                Console.WriteLine("The "+ CustomerDivider+ " is not divider for the "+ CustomerValue);
+            else Console.WriteLine("The "+ CustomerDivider+ " is divider for the "+ CustomerValue);
+            Console.Read();
+        }
+    }
+}
---
+++ RudTask1/RudTask1/RudTask1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RudTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int CustomerValue, CustomerDivider = 0;
+
+            //Get customer data
+            Console.Write("Hello! Insert your Value and press Enter.\n");
+            CustomerValue=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Insert possible divider for the "+ CustomerValue+" and press Enter.\n");
+            CustomerDivider= Convert.ToInt32(Console.ReadLine());
+
+            //Check if CustomerDivider is divider for CustomerValue
+            if (CustomerValue % CustomerDivider)
+                Console.WriteLine("The "+ CustomerDivider+ " is not divider for the "+ CustomerValue);
+            else Console.WriteLine("The "+ CustomerDivider+ " is divider for the "+ CustomerValue);
+
+            Console.Write("Press Enter for exit.");
+            Console.Read();
+        }
+    }
+}
